f551a539061d6d3b3966549d685955fdca04ecac
alireza <alirezaarzehgar82@gmail.com>
2023-11-28 21:30:59 +0330

    comment unit test error
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- util/api_test.go
+++ util/api_test.go
@@ -3,7 +3,7 @@ package util_test
 import "testing"
 
 func TestCreateSHA256(t *testing.T) {
-	t.Error("Fake error for test")
+	// t.Error("Fake error for test")
 }
 
 func TestCreateUserToken(t *testing.T) {
