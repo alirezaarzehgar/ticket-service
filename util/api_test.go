0b5500c91e742fc1265f44b92741e82d91856fd1
alireza <alirezaarzehgar82@gmail.com>
2023-11-28 20:39:29 +0330

    test commit for check CI
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

---
+++ util/api_test.go
@@ -0,0 +1,19 @@
+package util_test
+
+import "testing"
+
+func TestCreateSHA256(t *testing.T) {
+	t.Error("Fake error for test")
+}
+
+func TestCreateUserToken(t *testing.T) {
+
+}
+
+func TestGetToken(t *testing.T) {
+
+}
+
+func TestGetUserId(t *testing.T) {
+
+}
