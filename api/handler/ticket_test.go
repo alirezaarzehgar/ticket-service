6c0e659368b560f246c37637269310bee47e758d
alireza <alirezaarzehgar82@gmail.com>
2023-11-30 22:31:16 +0330

    test: just write null body corner case test for ticket handlers
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- api/handler/ticket_test.go
+++ api/handler/ticket_test.go
@@ -1,15 +1,24 @@
 package handler_test
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+
+	"github.com/alirezaarzehgar/ticketservice/api/handler"
+)
+
+func TestUploadAsset(t *testing.T) {
+	nilBodyTest(t, handler.UploadAsset, http.MethodPost, "/ticket/asset")
+}
 
 func TestSendTicket(t *testing.T) {
-
+	nilBodyTest(t, handler.CreateOrganization, http.MethodPost, "/organization/new")
 }
 
 func TestGetAllTickets(t *testing.T) {
-
+	nilBodyTest(t, handler.CreateOrganization, http.MethodPost, "/organization/new")
 }
 
 func TestReplyToTicket(t *testing.T) {
-
+	nilBodyTest(t, handler.ReplyToTicket, http.MethodPost, "/ticket/1/mail")
 }
